81d4758c920dab5033d85721788d881708e73962
Naim Jeem <naim36208@gmail.com>
2017-12-18 13:34:18 +0600

    Validation on submit

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -19,6 +19,18 @@ export class SignupFormComponent implements OnInit {
     password: new FormControl('', Validators.required)
   });
 
+
+  login() {
+    // const isValid = authService.login(this.form.value);
+    // if (!isValid) {
+    //   this.form.setErrors({
+    //     inValidLogin: true
+    //   });
+    // }
+    this.form.setErrors({
+      inValidLogin: true
+    });
+  }
   get username() {
     return this.form.get('username');
   }
